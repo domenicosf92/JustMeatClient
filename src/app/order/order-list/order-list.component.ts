096ed1f05f4a2349525baa56c22928ca075049e0
domenicosf92 <signorefioredomenico@tiscali.it>
2020-01-03 11:36:58 +0100

    Added filter by userId in order-list component.ts, must implement html

---
+++ src/app/order/order-list/order-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { Order } from 'modules/orderInterface';
+import { AuthService } from '../../auth.service';
+import * as jwt_decode from 'jwt-decode';
+
+@Component({
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.css']
+})
+export class OrderListComponent implements OnInit {
+  userId: string;
+  userOrders: Array<Order>;
+
+  constructor(private orderService: OrderService, private auth: AuthService) { }
+
+  async ngOnInit() {
+    const token = this.auth.getToken();
+    const decoded = jwt_decode(token) as any;
+    this.userId = decoded.subject;
+    this.userOrders =  await this.orderService.getOrdersByUserId(this.userId);
+  }
+
+}
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Order } from '../../../modules/orderInterface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  orderUrl = 'http://localhost:3006/orders';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getOrdersByUserId(userId: string): Promise<Order[]> {
+    return this.httpClient.get<Order[]>(`${this.orderUrl}?userId=${userId}`).toPromise();
+  }
+  public getOrders(): Promise<Order[]> {
+    return this.httpClient.get<Order[]>(`${this.orderUrl}`).toPromise();
+  }
 }
