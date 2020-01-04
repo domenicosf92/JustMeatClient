4f81585e9490732bdd6afe84493513a3b7259a66
stealth90 <41144486+stealth90@users.noreply.github.com>
2020-01-04 14:03:06 +0100

    added functionality for create new order before send order itself

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './filter-restaurants/filter.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit} from '@angular/core';
 import { RestaurantsService } from '../restaurants.service';
-import { Restaurant } from '../restaurants.model';
+import { Restaurant, Plate } from '../restaurants.model';
 import {ActivatedRoute} from '@angular/router';
+import { Order, OrderList } from 'modules/orderInterface';
+import * as jwt_decode from 'jwt-decode';
+import { AuthService } from 'src/app/auth.service';
 
 
 @Component({
@@ -11,6 +14,8 @@ import {ActivatedRoute} from '@angular/router';
 })
 
 export class RestaurantComponent implements OnInit {
+  order : Order ;
+  plates : OrderList[] = [];
   restaurant: Restaurant = {
     id: '',
     name: '',
@@ -25,11 +30,46 @@ export class RestaurantComponent implements OnInit {
     typology: ''
   };
   public restaurantName: string;
+  userId: string;
   constructor(public restaurantsService: RestaurantsService ,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private auth: AuthService) {}
 
   async ngOnInit() {
     this.restaurantName = this.route.snapshot.paramMap.get('restaurant');
     this.restaurant = await this.restaurantsService.getRestaurantsByName(this.restaurantName);
+    const token = this.auth.getToken();
+    const decoded = jwt_decode(token) as any;
+    this.userId = decoded.subject;
+  }
+
+  addPlate(plate:OrderList){
+    this.plates.push(plate);
+  }
+  deletePlate(plate:OrderList){
+    var index =this.plates.findIndex((i,index) =>{
+      if(i.namePlate ==plate.namePlate){
+       return index}
+    });
+    this.plates.splice(index,1);
+  }
+  getTotalAmount(){
+    var totalAmount=0;
+    for(let item of this.plates){
+      totalAmount +=item.price;
+    }
+    return totalAmount;
+  }
+
+  newOrder(order : Order){
+    var timestampNow = new Date();
+    var date = timestampNow.getDate() + "-" + (timestampNow.getMonth()+1) + "-" + timestampNow.getFullYear();
+    var time = timestampNow.getHours() + ":" + timestampNow.getMinutes() + ":" + timestampNow.getSeconds();
+    var dateOrder = date + " " + time;
+    var uuid = new uuid();
+    order.date = dateOrder;
+    order.id =uuid;
+    order.restaurantId = this.restaurant.id;
+    order.userId = this.userId;
   }
 }
